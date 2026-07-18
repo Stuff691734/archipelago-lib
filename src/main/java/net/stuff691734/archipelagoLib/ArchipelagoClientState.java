f4e086fc3a60a0ebe21fb08cfd5c9d9bd6af1817
Stuff691734 <23fuelln@gmail.com>
2026-07-17 20:28:07 -0500

    add documentation to ArchipelagoClientState

--- src/main/java/net/stuff691734/archipelagoLib/ArchipelagoClientState.java
+++ src/main/java/net/stuff691734/archipelagoLib/ArchipelagoClientState.java
@@ -13,19 +13,31 @@ public class ArchipelagoClientState implements StorageInterface {
         this.checks = new HashSet<>();
     }
 
+    @Override
     public boolean hasCheck(String checkName) {
         return this.checks.contains(checkName);
     }
 
+    /**
+     * Adds a check to the list of received checks.
+     * @param checkName the name of the check to add.
+     */
     public void addCheck(String checkName) {
         this.checks.add(checkName);
     }
 
+    /**
+     * Sets the list of received checks.
+     * @param checks the checks for the new list to have
+     */
     public void setChecks(String[] checks) {
         this.clear();
         this.checks.addAll(Arrays.asList(checks));
     }
 
+    /**
+     * Clears the list of received checks.
+     */
     public void clear() {
         this.checks.clear();
     }
